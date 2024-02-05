808d4e86e30c343cca810951ff93023d4a8f2394
James Runnalls <runnalls.james@gmail.com>
2024-02-05 10:36:27 +0100

    Remove loggin

--- src/pages/home/functions.js
+++ src/pages/home/functions.js
@@ -87,10 +87,9 @@ export const summariseData = (forecast, frozen) => {
     dt = [dtMin, dtMax];
     value = [4, 4];
   } else {
-    var day;
     for (let i = 0; i < forecast.date.length; i++) {
       if (forecast.date[i] > dtMin) {
-        day = formatDate(forecast.date[i]);
+        let day = formatDate(forecast.date[i]);
         let v = forecast.value[i];
         if (v !== null) {
           dt.push(new Date(forecast.date[i]));
@@ -98,10 +97,6 @@ export const summariseData = (forecast, frozen) => {
           summary[day].push(v);
         }
       }
-    }
-    if (summary[formatDate(dtMin + 4 * 86400000)].length === 0) {
-      summary[formatDate(dtMin + 4 * 86400000)] =
-        summary[formatDate(dtMin + 3 * 86400000)];
     }
   }
   for (let key in summary) {
