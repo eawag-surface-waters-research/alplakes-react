dd7f2e73c130027598472def7fce95dd0819b2ab
James Runnalls <runnalls.james@gmail.com>
2024-03-09 14:04:56 +0100

    Graph edits

--- src/pages/lake/graph.jsx
+++ src/pages/lake/graph.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
+import Toggle from "../../components/sliders/toggle";
 
 class Graph extends Component {
+  state = {
+    annual: false,
+  };
+  toggleAnnual = () => {
+    this.setState({ annual: !this.state.annual });
+  };
   render() {
+    var { annual } = this.state;
     return (
       <React.Fragment>
-        <div className="settings">Graph settings here</div>
-        <div className="view-area">Graphs here</div>
+        <div className="settings">
+          <Toggle
+            left="Timeframe"
+            right="Annual"
+            onChange={this.toggleAnnual}
+            checked={annual}
+          />
+        </div>
+        <div className="view-area">
+          <div className="graph-area"></div>
+        </div>
       </React.Fragment>
     );
   }
