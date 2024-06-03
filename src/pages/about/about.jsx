769e0c2eef97f24cbb333ccb477474e4eac8341f
James Runnalls <runnalls.james@gmail.com>
2024-06-03 13:59:12 +0200

    Add references

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -94,7 +94,7 @@ class About extends Component {
     ];
     return (
       <div className="main">
-        <NavBar {...this.props} relative={true}/>
+        <NavBar {...this.props} relative={true} />
         <div className="about">
           <div className="section-content">
             <div className="header">
@@ -199,84 +199,123 @@ class About extends Component {
           <div className="section-content">
             <div className="sub-header">Model development</div>
             <p>
+              Irani Rahaghi, A., Odermatt, D., Anneville, O., Sepúlveda Steiner,
+              O., Reiss, R. S., Amadori, M., ... & Bouffard, D. (2024). Combined
+              Earth observations reveal the sequence of conditions leading to a
+              large algal bloom in Lake Geneva. Communications Earth &
+              Environment, 5(1), 229. https://doi.org/10.1038/s43247-024-01351-5
+            </p>
+            <p>
+              Tian, W., Zhang, Z., Bouffard, D., Wu, H., Xin, K., Gu, X., &
+              Liao, Z. (2024). Enhancing interpretability and generalizability
+              of deep learning-based emulator in three-dimensional lake
+              hydrodynamics using Koopman operator and transfer learning:
+              Demonstrated on the example of lake Zurich. Water Research, 249,
+              120996. https://doi.org/10.1016/j.watres.2023.120996
+            </p>
+            <p>
               Safin, A., Bouffard, D., Ozdemir, F., Ramón, C. L., Runnalls, J.,
               Georgatos, F., ... & Šukys, J. (2022). A Bayesian data
               assimilation framework for lake 3D hydrodynamic models with a
-              physics-preserving particle filtering method using SPUX-MITgcm
-              v1. Geoscientific Model Development, 15(20), 7715-7730.
+              physics-preserving particle filtering method using SPUX-MITgcm v1.
+              Geoscientific Model Development, 15(20), 7715-7730.
+              https://doi.org/10.5194/gmd-15-7715-2022
             </p>
             <p>
               Li, C., Odermatt, D., Bouffard, D., Wüest, A., & Kohn, T. (2022).
               Coupling remote sensing and particle tracking to estimate
-              trajectories in large water bodies. International Journal of
-              Applied Earth Observation and Geoinformation, 110, 102809.
+              trajectories in large water bodies. International Journal of
+              Applied Earth Observation and Geoinformation, 110, 102809.
+              https://doi.org/10.1016/j.jag.2022.102809
             </p>
             <p>
               Baracchini, T., Hummel, S., Verlaan, M., Cimatoribus, A., Wüest,
               A., & Bouffard, D. (2020). An automated calibration framework and
-              open source tools for 3D lake hydrodynamic models. Environmental
-              Modelling & Software, 134, 104787
+              open source tools for 3D lake hydrodynamic models. Environmental
+              Modelling & Software, 134, 104787
+              https://doi.org/10.1016/j.envsoft.2020.104787
             </p>
             <p>
               Baracchini, T., Wüest, A., & Bouffard, D. (2020). Meteolakes: An
               operational online three-dimensional forecasting platform for lake
-              hydrodynamics. Water research, 172, 115529.
+              hydrodynamics. Water research, 172,
+              115529.https://doi.org/10.1016/j.watres.2020.115529
             </p>
             <p>
               Baracchini, T., Chu, P. Y., Šukys, J., Lieberherr, G., Wunderle,
               S., Wüest, A., & Bouffard, D. (2020). Data assimilation of in situ
               and satellite remote sensing data to 3D hydrodynamic lake models:
-              a case study using Delft3D-FLOW v4. 03 and OpenDA v2.
-              4. Geoscientific Model Development, 13(3), 1267-1284.
-            </p>
-            <div className="sub-header">Remote sensing development</div>
-            <p>
-              Sepúlveda Steiner, O., Forrest, A. L., McInerney, J. B., Fernández
-              Castro, B., Lavanchy, S., Wüest, A., & Bouffard, D. (2023).
-              Spatial variability of turbulent mixing from an underwater glider
-              in a large, deep, stratified lake. Journal of Geophysical
-              Research: Oceans, 128(6), e2022JC018913.
+              a case study using Delft3D-FLOW v4. 03 and OpenDA v2. 4.
+              Geoscientific Model Development, 13(3), 1267-1284.
+              https://doi.org/10.5194/gmd-13-1267-2020
             </p>
             <p>
-              Perolo, P., Fernández Castro, B., Escoffier, N., Lambert, T.,
-              Bouffard, D., & Perga, M. E. (2021). Accounting for surface waves
-              improves gas flux estimation at high wind speed in a large
-              lake. Earth System Dynamics, 12(4), 1169-1189.
-            </p>
-            <p>
-              Råman Vinnå, L., Bouffard, D., Wüest, A., Girardclos, S., &
-              Dubois, N. (2020). Assessing subaquatic mass movement hazards: an
-              integrated observational and hydrodynamic modelling
-              approach. Water Resources Management, 34(13), 4133-4146.
-            </p>
-            <p>
-              Nouchi, V., Kutser, T., Wüest, A., Müller, B., Odermatt, D.,
-              Baracchini, T., & Bouffard, D. (2019). Resolving biogeochemical
-              processes in lakes using remote sensing. Aquatic Sciences, 81,
-              1-13.
-            </p>
-            <p>
-              Soomets, T., Kutser, T., Wüest, A., & Bouffard, D. (2019). Spatial
-              and temporal changes of primary production in a deep peri-alpine
-              lake. Inland Waters, 9(1), 49-60.
+              Gaudard, A., Råman Vinnå, L., Bärenbold, F., Schmid, M., &
+              Bouffard, D. (2019). Toward an open access to high-frequency lake
+              modeling and statistics data for scientists and practitioners–the
+              case of Swiss lakes using Simstrat v2. 1. Geoscientific Model
+              Development, 12(9), 3955-3974.
             </p>
             <p>
               Baracchini, T., Bärenzung, K., Bouffard, D., & Wüest, A. J.
               (2019). Le Lac de Zurich en Ligne-Prévisions hydrodynamiques 3D en
               temps-réel sur meteolakes. ch. Aqua & Gas-Fachzeitschrift für Gas,
-              Wasser und Abwasser. Aqua & Gas, 12, 24-29.
+              Wasser und Abwasser. Aqua & Gas, 12, 24-29.
+            </p>
+            <p>
+              Goudsmit, G. H., Burchard, H., Peeters, F., & Wüest, A. (2002).
+              Application of k‐ϵ turbulence models to enclosed basins: The role
+              of internal seiches. Journal of Geophysical Research: Oceans,
+              107(C12), 23-1.
+            </p>
+            <div className="sub-header">Remote sensing development</div>
+            <p>
+              Soomets, T., Kutser, T., Wüest, A., & Bouffard, D. (2019). Spatial
+              and temporal changes of primary production in a deep peri-alpine
+              lake. Inland Waters, 9(1), 49-60.
+              https://doi.org/10.1080/20442041.2018.1530529
+            </p>
+            <div className="sub-header">Framework use</div>
+            <p>
+              Sepúlveda Steiner, O., Forrest, A. L., McInerney, J. B., Fernández
+              Castro, B., Lavanchy, S., Wüest, A., & Bouffard, D. (2023).
+              Spatial variability of turbulent mixing from an underwater glider
+              in a large, deep, stratified lake. Journal of Geophysical
+              Research: Oceans, 128(6), e2022JC018913.
+              https://doi.org/10.1029/2022JC018913
+            </p>
+            <p>
+              Perolo, P., Fernández Castro, B., Escoffier, N., Lambert, T.,
+              Bouffard, D., & Perga, M. E. (2021). Accounting for surface waves
+              improves gas flux estimation at high wind speed in a large lake.
+              Earth System Dynamics, 12(4), 1169-1189.
+              https://doi.org/10.5194/esd-12-1169-2021
+            </p>
+            <p>
+              Råman Vinnå, L., Bouffard, D., Wüest, A., Girardclos, S., &
+              Dubois, N. (2020). Assessing subaquatic mass movement hazards: an
+              integrated observational and hydrodynamic modelling approach.
+              Water Resources Management, 34(13), 4133-4146.
+              https://doi.org/10.1007/s11269-020-02660-y
+            </p>
+            <p>
+              Nouchi, V., Kutser, T., Wüest, A., Müller, B., Odermatt, D.,
+              Baracchini, T., & Bouffard, D. (2019). Resolving biogeochemical
+              processes in lakes using remote sensing. Aquatic Sciences, 81,
+              1-13. https://doi.org/10.1007/s00027-019-0626-3
             </p>
             <p>
               Bouffard, D., Kiefer, I., Wüest, A., Wunderle, S., & Odermatt, D.
               (2018). Are surface temperature and chlorophyll in a large deep
               lake related? An analysis based on satellite observations in
-              synergy with hydrodynamic modelling and in-situ data. Remote
-              sensing of environment, 209, 510-523.
+              synergy with hydrodynamic modelling and in-situ data. Remote
+              sensing of environment, 209, 510-523.
+              https://doi.org/10.1016/j.rse.2018.02.056
             </p>
             <p>
               Råman Vinnå, L., Wüest, A., & Bouffard, D. (2017). Physical
-              effects of thermal pollution in lakes. Water Resources
-              Research, 53(5), 3968-3987.
+              effects of thermal pollution in lakes. Water Resources Research,
+              53(5), 3968-3987. https://doi.org/10.1002/2016WR019686
             </p>
           </div>
         </div>
