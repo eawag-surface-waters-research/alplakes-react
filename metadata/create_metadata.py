f8b77b6bc7cb3514b955a0cb5ad4439be93af2e9
James Runnalls <runnalls.james@gmail.com>
2026-05-28 12:08:03 +0200

    Add Neuchatel mitgcm model

--- metadata/create_metadata.py
+++ metadata/create_metadata.py
@@ -5,7 +5,7 @@ import requests
 import functions as func
 
 upload = True
-bucket_folder = "static/website/metadata/master"
+bucket_folder = "static/website/metadata/neuchatel"
 
 # Load Metadata
 with open("metadata.json") as f:
